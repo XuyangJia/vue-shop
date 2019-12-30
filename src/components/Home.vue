e868928d466e3f0c80d2db52617587795337fd61
Xuyang Jia <jiaxuyang@outlook.com>
2019-12-31 02:21:31 +0800

    主界面布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>home</div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/logo.png">
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航二</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,4 +47,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background: #373d41;
+  padding-left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background: #333744;
+}
+.el-main {
+  background: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import { Button, Input, Form, FormItem, Message } from 'element-ui'
+import {
+  Button,
+  Input,
+  Form,
+  FormItem,
+  Message,
+  Menu,
+  Submenu,
+  MenuItem,
+  Container,
+  Header,
+  Aside,
+  Main
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Input)
 Vue.use(Form)
 Vue.use(FormItem)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
